66206ae6f44c0eab806285dfca105caf9914f48a
a-moshchenko <alexssssf@gmail.com>
2019-07-23 01:13:58 +0300

    [ADD] add post from homepage but is not working now

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.views import generic
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.utils import timezone
 
 
 class PostList(generic.ListView):
@@ -11,3 +15,17 @@ class PostList(generic.ListView):
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.created_on = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'post_edit.html', {'form': form})

9c31a968a54a4c59bf032ae52c24051b87b7b4fb
a-moshchenko <alexssssf@gmail.com>
2019-07-28 01:05:32 +0300

    [ADD] fix add post, add some change in html, but search not work(((

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('search/', views.SearchResultsView.as_view(), name='search_results'),
     path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,10 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
 from .models import Post
 from .forms import PostForm
-from django.shortcuts import render
 from django.shortcuts import redirect
 from django.utils import timezone
 from django.db.models import Q
+from django.shortcuts import render, get_object_or_404
 
 
 class PostList(ListView):
@@ -13,9 +13,9 @@ class PostList(ListView):
     paginate_by = 4
 
 
-class PostDetail(DetailView):
-    model = Post
-    template_name = 'post_detail.html'
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'post_detail.html', {'post': post})
 
 
 class SearchResultsView(ListView):
@@ -41,4 +41,19 @@ def post_new(request):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
+    return render(request, 'post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
     return render(request, 'post_edit.html', {'form': form})
